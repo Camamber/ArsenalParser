1d45f384ad251c441450186e78f465d6ef05bfd5
Camamber <egorka.k2009@gmail.com>
2021-07-13 22:43:14 +0300

    ss

--- index.js
+++ index.js
@@ -10,22 +10,34 @@ async function parse() {
     const urls = fs.readFileSync(process.argv[2]).toString().split('\n')
     const result = []
     let index = 0;
-    for (const url of urls) {
-        const response = await request.get(url.trim(), {
-            headers: {
-                'User-Agent': 'LocomotiveBot/1.0'
-            }
-        }).then(body => HTMLParser.parse(body))
+    for (let i = 0; i < urls.length; i += 5) {
+        const chunk = urls.slice(i, i + 5).map(url => url.trim());
 
-        const items = response.querySelectorAll(process.argv[4])
-        for (const item of items) {
-            if (process.argv.length == 5) {
-                result.push(`${item.text.trim()}\t${url}`)
-            } else {
-                result.push(`${item.attributes[process.argv[5]].trim()}\t${url}`)
+        const requests = chunk.map(
+            url => request.get(url, { headers: { 'User-Agent': 'LocomotiveBot/1.0' } })
+                .then(body => {
+                    return { url, document: HTMLParser.parse(body) }
+                })
+                .catch(err => {
+                    console.log(url, err)
+                    return null
+                })
+        )
+        const responses = (await Promise.all(requests)).filter(item => !!item)
+
+        for (const { url, document } of responses) {
+            const items = document.querySelectorAll(process.argv[4])
+            for (const item of items) {
+                if (process.argv.length == 5) {
+                    result.push(`${item.text.trim()}\t${url}`)
+                } else {
+                    result.push(`${item.attributes[process.argv[5]].trim()}\t${url}`)
+                }
             }
+            console.log(++index, '/', urls.length, url.trim(), 'parsed')
         }
-        console.log(++index, '/', urls.length, url, 'parsed')
     }
+
     fs.writeFileSync(process.argv[3], result.join('\n'), { encoding: 'utf-8' })
 }
+
